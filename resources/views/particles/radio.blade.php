172b36cc27d551f8193ac60e26fc7fc417d7906d
Agassi <agasi_dark@ukr.net>
2018-07-23 18:43:14 +0300

    fix

--- resources/views/particles/radio.blade.php
+++ resources/views/particles/radio.blade.php
@@ -11,5 +11,4 @@
             @endforeach
         @endif
     </ul>
-
 </div>
---
+++ src/Widget.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Akopean\widgetsGroup;
+
+class Widget{
+ 
+	protected $widgets;
+ 
+
+	public function __construct(){
+		$this->widgets = config('widgets');
+	}
+ 
+     public static function routes()
+    {
+        require __DIR__.'/../routes/routes.php';
+    }
+
+}
